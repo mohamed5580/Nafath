4fb220e4ffa8ae40d1f5fb2f15161e0f33a77bf6
Mohamed Ahmed <125946649+mohamedahmed5580@users.noreply.github.com>
2025-06-25 18:50:35 +0300

    Add project files.

---
+++ Nafath/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nafath.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
