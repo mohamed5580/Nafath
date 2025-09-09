4b9cc4285eeda8286a25a2f24ef9e608f5131675
Mohamed Ahmed <125946649+mohamedahmed5580@users.noreply.github.com>
2025-09-09 17:23:49 +0300

    update mm

--- Nafath/Services/EmailSender.cs
+++ Nafath/Services/EmailSender.cs
@@ -1,13 +1,31 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net;
+using System.Net.Mail;
 
 namespace Nafath.Services
 {
-    public class EmailSender : IEmailSender
+    public class EmailSender : IEmailSender 
     {
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            // your SMTP or third‑party logic here
-            return Task.CompletedTask;
+            var fMail = "mohamed.5580.558@gmail.comm";
+            var fPassword = "Beko 2025";
+
+            var theMsg = new MailMessage();
+            theMsg.From = new MailAddress(fMail);
+            theMsg.Subject = subject;
+            theMsg.To.Add(email);
+            theMsg.Body = $"<html><body>{htmlMessage}</body></html>";
+            theMsg.IsBodyHtml = true;
+
+            var smtpClint = new SmtpClient("smtp-mail.outlook.com")
+            {
+                EnableSsl = true,
+                Credentials = new NetworkCredential(fMail, fPassword),
+                Port = 587
+            };
+
+            smtpClint.Send(theMsg);
         }
     }
 }
